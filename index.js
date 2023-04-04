92fab920b26e2d4749ee775699186396ce488353
Jeet <jeetbhadaniya1228@gmail.com>
2023-04-04 12:12:10 +0530

    aletr added

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const cors = require("cors");
 require('dotenv').config()
 const app = express();
 app.use(cors());
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+  });
 app.use(express.urlencoded({ extended: false }));
 const jsonParser = express.json();
 app.use(jsonParser);
